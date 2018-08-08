08b87e554c001f1759d72734dbae543145262d7d
Simon Deeb <simon19-meet@meet.mit.edu>
2018-08-08 08:13:15 -0700

    add done

--- exercises/app.py
+++ exercises/app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,4 +10,10 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
+@app.route('/add', methods=['GET','POST'])
+def add_student_route():
+    if request.method == 'GET':
+        return render_template('add.html')
+    return render_template('add.html', a="Received POST request")
 app.run(debug=True)
+
